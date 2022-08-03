420fb6ef37d1a0bab1e45c2c0dccaaf3c94b030a
Engin İNSEL <76450122+enqinsel@users.noreply.github.com>
2022-08-03 21:40:04 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+
+console.log("hi!");
+console.log(1234);
+console.log(true);
+console.log([1,2,3]);
+console.error("An error occured");
+console.warn("A warning has occured");
+
+//tek satırlık yorum satırı
+/* çoklu yorum satırları */
+
+console.clear(); // tüm console temizler
